ca9439280fee363a9b75c642e88458b74c8bbda8
Oleg Orlov <orelcokolov@gmail.com>
2022-03-13 01:58:11 +0300

    Try to refactor structure a bit

--- samples/tk.rb
+++ samples/tk.rb
@@ -1,7 +1,10 @@
 require 'tk'
-root = TkRoot.new { title "Hello, World!" }
-TkLabel.new(root) do
-   text 'Hello, World!'
-   pack { padx 15 ; pady 15; side 'left' }
+
+if not defined?(Ocra)
+   root = TkRoot.new { title "Hello, World!" }
+   TkLabel.new(root) do
+      text 'Hello, World!'
+      pack { padx 15 ; pady 15; side 'left' }
+   end
+   Tk.mainloop
 end
-Tk.mainloop
